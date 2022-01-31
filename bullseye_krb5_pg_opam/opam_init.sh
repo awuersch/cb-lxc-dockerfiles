0f92a4ead998862ecca7ee2ea275df0b99984dff
Tony Wuersch <twuersch@omnifederal.com>
2022-01-30 19:44:40 -0500

    opam dockerfiles fixed

--- bullseye_krb5_pg_opam/opam_init.sh
+++ bullseye_krb5_pg_opam/opam_init.sh
@@ -4,4 +4,4 @@ set -euf -o pipefail
 
 OPAMREPO=git/github.com/ocaml/opam-repository
 # we disable opam sandboxing to allow docker setup
-cd ~ && opam init --disable-sandboxing -a -y ~/$OPAMREPO --comp 4.07.0
+cd ~ && opam init --disable-sandboxing -a -y ~/$OPAMREPO --comp 4.13.1
